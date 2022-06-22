15201c4226ee14888fa01bf7800f62f568e056bc
Tree <1332906266@qq.com>
2022-06-22 11:58:21 +0800

    views页面添加

---
+++ src/views/home/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>11</div>
+</template>
+
+<script lang="ts">
+import {defineComponent} from "vue";
+
+const Home = defineComponent({
+  setup() {
+    return {}
+  },
+  components: {}
+})
+export default Home;
+</script>
