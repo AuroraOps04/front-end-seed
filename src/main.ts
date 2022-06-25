0250d3a7f45964a7c6a4581612bcfbc581e212c6
kominic <543616401@qq.com>
2022-06-25 17:13:02 +0800

    配置文件，页面图片

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,26 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
 import router from '@/router/index'
 import store from '@/store/index'
 
-createApp(App).use(router).use(store).mount('#app')
+
+// 新增代码：引入全部组件及样式
+import ElementPlus from 'element-plus'
+import "element-plus/dist/index.css";
+
+// 引入 echarts
+import * as echarts from 'echarts'
+const app = createApp(App)
+// 全局挂载 echarts
+app.config.globalProperties.$echarts = echarts
+
+
+
+app.use(store)
+app.use(router)
+
+// 新增代码：注册全部组件
+app.use(ElementPlus)
+
+app.mount('#app')
+
