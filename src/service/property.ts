78a07d9deb9a33dd50077a2dafe3855564260862
kominic <543616401@qq.com>
2022-07-15 15:08:42 +0800

    07.15 账号管理接口

--- src/service/property.ts
+++ src/service/property.ts
@@ -2,7 +2,11 @@ import request from '@service/_requests'
 import AreaQuery = API.AreaQuery
 import PageParams = API.PageParams
 import AreaQueryParams = API.AreaQueryParams
+import CategoryQueryParams = API.CategoryQueryParams
+import CategoryParams = API.CategoryParams
+import AreaParams = API.AreaParams
 
+// 地区管理接口
 export const removeAreaBatchByIdsApi = (params: number[]): Promise<API.Response> => {
   return request('/area/removeAreaBatchByIds', {
     method: 'DELETE',
@@ -24,14 +28,54 @@ export const addAreaApi = (areaName: string): Promise<API.Response> => {
   })
 }
 
-export const editAreaByNameApi = (areaId: number, areaName: string): Promise<API.Response> => {
-  return request(`/area/editAreaByName/${areaName}`, {
-    method: 'POST'
+export const editAreaByNameApi = (params: AreaParams): Promise<API.Response> => {
+  return request(`/area/editAreaByName`, {
+    method: 'POST',
+    params
   })
 }
 
 export const listAreaByPageApi = (params: AreaQueryParams & PageParams): Promise<API.Response> => {
   return request('/area/getAreaByParam', {
+    method: 'GET',
+    params
+  })
+}
+
+// 分类管理接口
+
+export const removeCategoryBatchByIdsApi = (params: number[]): Promise<API.Response> => {
+  return request('/category/removeCategoryBatchByIds', {
+    method: 'DELETE',
+    params: {
+      ids: params
+    }
+  })
+}
+
+export const removeCategoryByIdApi = (categoryId: number): Promise<API.Response> => {
+  return request(`/category/deleteById/${categoryId}`, {
+    method: 'DELETE'
+  })
+}
+
+export const addCategoryApi = (categoryName: string): Promise<API.Response> => {
+  return request(`/category/addCategoryByName/${categoryName}`, {
+    method: 'GET'
+  })
+}
+
+export const editCategoryByNameApi = (params: CategoryParams): Promise<API.Response> => {
+  return request(`/category/editCategoryByName`, {
+    method: 'POST',
+    params
+  })
+}
+
+export const listCategoryByPageApi = (
+  params: CategoryQueryParams & PageParams
+): Promise<API.Response> => {
+  return request('/category/getCategoryByParam', {
     method: 'GET',
     params
   })
